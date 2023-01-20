44e971aea82d58f7429ea46ef92438fee021469e
Vsevolod Melnik <vsevolodmelnik@MacBook-Air-Vsevolod.local>
2023-01-20 20:13:49 +0300

    Created pin methods

---
+++ UI-View-pin/UI-View-pin/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  UI-View-pin
+//
+//  Created by Vsevolod Melnik on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        
+        let firstView = UIView()
+        firstView.backgroundColor = .red
+        
+        view.addSubview(firstView)
+        
+        firstView.pinLeft(to: view, 50)
+        firstView.pinTop(to: view, 50)
+        firstView.pinCenter(to: view)
+        
+    }
+
+
+}
+
